a0fd8e39c2e96924fed4ae202dc58df625fcf351
Max Datsenko <datsenko.md@gmail.com>
2023-11-08 13:17:58 +0200

    remove trash files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Hello from './Components/Hello.jsx';
 import HelloMessage from './Components/HelloMessage.jsx';
 import List from './Components/List.jsx';
